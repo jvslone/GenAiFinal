0aa9660e62b661d582e42c7076284fe705a6cabf
Yisi-Alisa-Yang <164107270+Yisi-Alisa-Yang@users.noreply.github.com>
2025-05-03 00:31:43 -0400

    update

--- test_complete.py
+++ test_complete.py
@@ -26,7 +26,7 @@ print(device)
 
 def load_data():
     use_rho_time = 0
-    # Input_File = 'Data/ChangeDataset622v2.pickle'
+    # Input_File = 'ChangeDataset622v1.pickle'
     Input_File = 'Data/3k.pickle'
     
     def ReadPickle(filename: str) -> dict:
